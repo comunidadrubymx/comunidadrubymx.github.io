fbdb4a05be7965cdceb1c982434af0e6764ee3c9
FerPerales <FerPerales@users.noreply.github.com>
2026-06-26 16:43:23 +0000

    deploy: ba9551108fdccc78ce894d2e2f4b7c8868902d3d

---
+++ _bridgetown/static/index-VMRQLRMD.js
@@ -0,0 +1,74 @@
+(() => {
+  // frontend/javascript/index.js
+  var upcomingEvent = {
+    year: 2025,
+    month: 6,
+    day: 26,
+    hours: 19,
+    minutes: 0,
+    seconds: 0
+  };
+  simplyCountdown("#countdown", {
+    year: upcomingEvent.year,
+    month: upcomingEvent.month,
+    day: upcomingEvent.day,
+    hours: upcomingEvent.hours,
+    minutes: upcomingEvent.minutes,
+    seconds: upcomingEvent.seconds,
+    words: {
+      days: { singular: " dia", plural: " dias" },
+      hours: { singular: " hora", plural: " horas" },
+      minutes: { singular: " minuto", plural: " minutos" },
+      seconds: { singular: " segundo", plural: " segundos" }
+    },
+    plural: true,
+    inline: false,
+    inlineClass: "simply-countdown-inline",
+    enableUtc: false,
+    onEnd: function() {
+      return;
+    },
+    refresh: 1e3,
+    sectionClass: "simply-section",
+    amountClass: "simply-amount",
+    wordClass: "simply-word",
+    zeroPad: false,
+    countUp: false
+  });
+  simplyCountdown("#countdown-en", {
+    year: upcomingEvent.year,
+    month: upcomingEvent.month,
+    day: upcomingEvent.day,
+    hours: upcomingEvent.hours,
+    minutes: upcomingEvent.minutes,
+    seconds: upcomingEvent.seconds,
+    words: {
+      days: { singular: " day", plural: " days" },
+      hours: { singular: " hour", plural: " hours" },
+      minutes: { singular: " minute", plural: " minutes" },
+      seconds: { singular: " second", plural: " seconds" }
+    },
+    plural: true,
+    inline: false,
+    inlineClass: "simply-countdown-inline",
+    enableUtc: false,
+    onEnd: function() {
+      return;
+    },
+    refresh: 1e3,
+    sectionClass: "simply-section",
+    amountClass: "simply-amount",
+    wordClass: "simply-word",
+    zeroPad: false,
+    countUp: false
+  });
+  window.toggleMenu = () => {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  };
+})();
+//# sourceMappingURL=index-VMRQLRMD.js.map
